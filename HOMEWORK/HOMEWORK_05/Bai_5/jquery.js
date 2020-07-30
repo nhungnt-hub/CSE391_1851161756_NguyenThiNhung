608bcf84dce58b71d968abf495d3d51fffde663e
Nguyen Thi Nhung <Email ca bn>
2020-07-30 22:11:25 +0700

    del

--- HOMEWORK/HOMEWORK_05/Bai_5/jquery.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("p").hide();
-    $("a").click(function () { 
-        var x = $(this).parent().parent().children().eq(2).show();
-        
-    });
-});
--- HOMEWORK/HOMEWORK_05/Bai_6/jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".login").click(function () { 
-        $(".loginDiv").toggleClass("loginShow");
-        
-    });
-    $(".menu-bar").click(function () { 
-        $(".form-inline").toggleClass("search-bar");
-    });
-});
